c20fbd4b66f8d1e38ceccd5974b486448ede0fda
Izuchukwu <izunduche29@gmail.com>
2025-03-16 14:35:27 +0100

    The Mobile Navigation has been added

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const acc = document.getElementsByClassName("box");
+const logo = document.getElementById("logo")
+const navLogo = document.getElementById("nav-logo")
+const openNav = document.getElementById("open-nav")
+const closeNav = document.getElementById("close-nav")
+const nav = document.getElementById("nav")
+// logo
+// nav-logo
+// open-nav
+// close-nav
+openNav.addEventListener('click' , () =>{
+  console.log('Werey')
+  openNav.classList = 'no--nav'
+  closeNav.classList = 'display'
+  logo.classList = 'no--nav'
+  navLogo.classList = 'display'
+  nav.classList = 'nav--bar'
+})
+closeNav.addEventListener('click' , () =>{
+  console.log('Werey')
+  closeNav.classList = 'no--nav'
+  openNav.classList = 'display'
+  logo.classList = 'display'
+  navLogo.classList = 'no--nav'
+  nav.classList = 'no--bg'
+})
 let i;
 console.log(acc)
 for (i = 0; i < acc.length; i++) {
